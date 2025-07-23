116a4b504ab8bfca4ac97343c49a91c172372144
Paulo-galvao <paulogalvaoj21@gmail.com>
2025-07-23 14:05:47 -0300

    ajustes para deploy 4

--- server/src/config/database.js
+++ server/src/config/database.js
@@ -7,8 +7,15 @@ const database = process.env.DATABASE_NAME;
 const host = process.env.HOST;
 
 const sequelize = new Sequelize(database, 'postgres', password, {
-  host,
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
   dialect: 'postgres',
+  dialectOptions: {
+      ssl: {
+        require: true, // Importante no Railway
+        rejectUnauthorized: false
+      }
+    }
 });
 
 
